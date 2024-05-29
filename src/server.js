103828366321487db154aa3eacbea7e38818bed1
PRoy <sam469roger@gmail.com>
2024-05-29 06:39:15 +0530

    implemented server, logger and resolved permission issue with logs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,37 @@
+import chalk from 'chalk';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
-import app from './app.js';
+import express from 'express';
+import morgan from 'morgan';
 
-const PORT = process.env.PORT || 4000;
+import { systemLogs, morganMiddleware } from './utils/logger.js';
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(morganMiddleware);
+
+app.get('/api/v1/test', (req, res) => {
+  res.status(200).json({
+    message: 'API is running...',
+  });
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `${chalk.green.bold('✔')} 👍 Server running in ${chalk.yellow.bold(
+      process.env.NODE_ENV,
+    )} mode on port ${chalk.blue.bold(PORT)}`,
+  );
+  systemLogs.info(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`,
+  );
 });
